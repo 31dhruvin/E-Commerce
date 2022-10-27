812bd4f455c9213e255d84356b97ce93e91613db
759135 <dhruvinmehta383@gmail.com>
2022-10-27 13:03:37 +0530

    changes done

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 import App from "./component/App";
 
-ReactDOM.render(<BrowserRouter basename='/E-Commerce'><App /></BrowserRouter>,document.getElementById("root"));
+ReactDOM.render(<HashRouter><App /></HashRouter>,document.getElementById("root"));
