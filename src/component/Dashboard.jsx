be8df7c2a163b44350306bfb86aa9473ce1d4245
759135 <dhruvinmehta383@gmail.com>
2022-10-27 13:27:53 +0530

    changed

--- src/component/Dashboard.jsx
+++ src/component/Dashboard.jsx
@@ -19,7 +19,7 @@ const Dashboard = () => {
         <div className="main">
             <section>
             
-            <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
+            {/* <div id="carouselExampleControls" className="carousel slide" data-bs-ride="carousel">
   <div className="carousel-inner">
     
     <div className="carousel-item">
@@ -37,6 +37,28 @@ const Dashboard = () => {
   <button className="carousel-control-next" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="next">
     <span className="carousel-control-next-icon" aria-hidden="true"></span>
     <span className="sr-only" >Next</span>
+  </button>
+</div> */}
+
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://img.freepik.com/free-vector/online-pharmacy-service_107791-2650.jpg?size=626&ext=jpg&ga=GA1.2.1646031593.1659596900" class="d-block w-50" alt="..."   />
+    </div>
+    <div class="carousel-item">
+      <img src="https://img.freepik.com/free-vector/pharmacists-holding-bottles-with-antibiotic-pills-vitamins-drugs-thermometer-ointment-tube-vector-illustration-drugstore-medical-store-pharmacy-pharmaceutical-business-concept_74855-10132.jpg?size=626&ext=jpg&ga=GA1.2.1646031593.1659596900" class="d-block w-50" alt="..." />
+    </div>
+    {/* <div class="carousel-item">
+      <img src="..." class="d-block w-100" alt="...">
+    </div> */}
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
   </button>
 </div>
             </section>
